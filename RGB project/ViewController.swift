166a15669d56314c3e87cc4032b07cb003137c14
William Henry Fulton IV <will.fulton95@gmail.com>
2017-06-10 23:51:17 -0400

    NSUserDefaults set. UI complete

--- RGB project/ViewController.swift
+++ RGB project/ViewController.swift
@@ -15,26 +15,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var colorSquare: UIView!
    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         updateBackgroundColor()
         colorSquare.layer.borderColor = UIColor.black.cgColor
         colorSquare.layer.borderWidth = 1
+   
+        let defaults = UserDefaults.standard
+        redSlider.value = defaults.float(forKey: "redSlider")
+        greenSlider.value = defaults.float(forKey: "greenSlider")
+        blueSlider.value = defaults.float(forKey: "blueSlider")
     
     }
    
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
-}
+    }
    
+    
     @IBAction func updateBackgroundColor() {
         let red = CGFloat(redSlider.value)
         let green = CGFloat(greenSlider.value)
         let blue = CGFloat(blueSlider.value)
-       
         colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+
+        let defaults = UserDefaults.standard
+        defaults.set(redSlider.value, forKey: "red")
+        defaults.set(greenSlider.value, forKey: "green")
+        defaults.set(blueSlider.value, forKey: "blue")
+        defaults.synchronize()
     }
+   
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "openColor") {
             let newViewController = segue.destination
